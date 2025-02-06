222878204b8474b2111e0470f9248a190e34462a
darlenemaebulaon <mdmmbulaon.student@ua.edu.ph>
2025-02-06 13:22:03 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+        <h2 className="name">Milagros Darlene Mae M. Bulaon</h2>
+        <div className="box">CPEITEL</div>
+    </div>
+  );
+};
+
+export default App;
